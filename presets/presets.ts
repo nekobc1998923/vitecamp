93a6cfe72c105eaf49bd10c6402a51d39e8f4148
菜猫子neko <815743831@qq.com>
2022-04-27 13:54:51 +0800

    fix: 修复开发环境下每次重新启动都生成eslintrc的问题

--- presets/presets.ts
+++ presets/presets.ts
@@ -34,7 +34,7 @@ export default (env: ConfigEnv) => {
       // Generate corresponding .eslintrc-auto-import.json file.
       // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
       eslintrc: {
-        enabled: true, // Default `false`
+        enabled: false, // Default `false`
         filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
         globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
       },
